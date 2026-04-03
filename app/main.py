0192ed5e7cb6dc202a4adee9c042aefdc2196af5
VioletHarmon2002 <liizasabanova@mail.ru>
2026-04-03 22:05:55 +0300

    allow cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]  # allow all origins, change later to real origins
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 my_name = "Liza"
 
 
