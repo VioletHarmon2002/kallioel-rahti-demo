5f27f3f3ed41696ac7cac095ac3e84a2f1844666
VioletHarmon2002 <liizasabanova@mail.ru>
2026-04-16 20:52:46 +0300

    created db

--- app/db.py
+++ app/db.py
@@ -7,15 +7,35 @@ def get_conn():
 
 def create_schema():
     with get_conn() as conn, conn.cursor() as cur:
+
+        
         cur.execute("""
         CREATE TABLE IF NOT EXISTS rooms (
             id SERIAL PRIMARY KEY,
             room_number INTEGER NOT NULL,
+            room_type VARCHAR,
             created_at TIMESTAMP NOT NULL DEFAULT now()
         );
         """)
 
+        
         cur.execute("""
-        ALTER TABLE rooms 
-        ADD COLUMN IF NOT EXISTS room_type VARCHAR;
+        CREATE TABLE IF NOT EXISTS hotel_guests (
+            id SERIAL PRIMARY KEY,
+            first_name VARCHAR NOT NULL,
+            last_name VARCHAR NOT NULL,
+            address VARCHAR NOT NULL
+        );
+        """)
+
+        
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS hotel_bookings (
+            id SERIAL PRIMARY KEY,
+            guest_key INTEGER REFERENCES hotel_guests(id),
+            room_id INTEGER REFERENCES rooms(id),
+            datefrom TIMESTAMP NOT NULL,
+            dateto TIMESTAMP NOT NULL,
+            add_info VARCHAR
+        );
         """)
